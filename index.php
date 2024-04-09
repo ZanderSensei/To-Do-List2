5bfe88f1850e1fcea248c50a1db55fc1917a44e6
ZanderSensei <109417236+ZanderSensei@users.noreply.github.com>
2024-04-09 17:46:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:zarserver2.database.windows.net,1433; Database = ZarApp2", "zander", "Zar12345");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "zander", "pwd" => "Zar12345", "Database" => "ZarApp2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:zarserver2.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
 // Environment variables for database connection
 $host = getenv('DB_HOST') ?: 'localhost'; // Using 'localhost' as a fallback
 $dbname = getenv('DB_NAME');
